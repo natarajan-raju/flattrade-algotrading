fcace5798e07c3226648162bb2cb50769b6f49ff
natarajan-raju <natarajan.raju90@gmail.com>
2024-11-24 10:36:08 +0530

    Reset job now perfomed 2 times a day

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -2,22 +2,25 @@ module.exports = {
     /**
      * Cron job to reset investment variables every day at 5:00 PM.
      */
-    dailyResetJob: {
+    dailyMorningJob: {
       task: async ({ strapi }) => {
         await strapi.service('api::variable.variable').resetInvestmentVariables();
         await strapi.service('api::contract.contract').clearContractVariables();
-      },
-      options: {
-        rule: "15 15 * * *", // Every day at 3:15 PM
-        tz: "Asia/Kolkata",  // Set to your desired timezone
-      },
-    },
-    dailyStopJob: {
-      task: async ({ strapi }) => {
         await strapi.service('api::variable.variable').stopTrading(1);
       },
       options: {
-        rule: "15 15 * * *", // Every day at 3:15 PM
+        rule: "07 00 * * *", // Every day at 7:00 AM
+        tz: "Asia/Kolkata",  // Set to your desired timezone
+      },
+    },
+    dailyEveningJob: {
+      task: async ({ strapi }) => {
+        await strapi.service('api::variable.variable').resetInvestmentVariables();
+        await strapi.service('api::contract.contract').clearContractVariables();
+        await strapi.service('api::variable.variable').stopTrading(1);
+      },
+      options: {
+        rule: "15 15 * * *", // Every day at 3:15 pm
         tz: "Asia/Kolkata",  // Set to your desired timezone
       },
     },
