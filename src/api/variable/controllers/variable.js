f561c92247cc10614cf5edc2d1327d07dcafdf6e
natarajan-raju <natarajan.raju90@gmail.com>
2025-03-17 23:02:22 +0530

    test3

--- src/api/variable/controllers/variable.js
+++ src/api/variable/controllers/variable.js
@@ -228,7 +228,7 @@ module.exports = createCoreController('api::variable.variable', ({ strapi }) => 
         //   if (!isBetween915And930()) {
         //     return ctx.send("Trading can only start between 9:15 and 9:30 AM.");
         //   }    
-          const indexItem = strapi.db.query('api::variable.variable').findOne({where: {indexToken}});
+          const indexItem =await strapi.db.query('api::variable.variable').findOne({where: {indexToken}});
           const index = indexItem.index;
           console.log(indexItem);
           let avoid = null;    
